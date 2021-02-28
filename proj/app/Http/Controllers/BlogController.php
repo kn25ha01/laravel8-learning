d35c0d3fd3589a20676945aba07573e7a7fea036
kn25ha01 <kn25ha01@gmail.com>
2021-02-28 21:32:27 +0900

    add blog

---
+++ proj/app/Http/Controllers/BlogController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Blog;
+
+class BlogController extends Controller
+{
+    /**
+     * ブログ一覧を表示する
+     * 
+     * @return view
+     */    
+    public function showList()
+    {
+        $blogs = Blog::all();
+        //dd($blogs);
+        return view('blog.list', ['blogs'=>$blogs]);
+    }
+}
--- proj/routes/web.php
+++ proj/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+// ブログ一覧画面を表示
+//Route::get('/', 'BlogController@showList')->name('blogs');
+Route::get(
+    '/',
+    [BlogController::class, 'showList']
+)->name('blogs');
+//$url = route('blogs');
+//return redirect()->route('blogs');
